9f70eb9b555b8e88d2332be72cd343f9d33245e6
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2024-11-27 22:40:04 +0900

    feat: add account deletion, profile retrieval, and address handling in signup

--- router/userRouter.js
+++ router/userRouter.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // controller
-const { login, register, user_delete, checkDuplicateId } = require('../controller/user');
+const { login, register, user_delete, profile, checkDuplicateId } = require('../controller/user');
 const { authenticateAccessToken } = require('../middlewares');
 
 router.post('/login', login);
 router.post('/register', register);
 router.delete('/delete', authenticateAccessToken, user_delete);
+router.get('/profile', authenticateAccessToken, profile);
 router.post('/checkDuplicate_id', checkDuplicateId);
 
 module.exports = router;
