397954795724c1a0bca12a5a5d7e3026c9da2172
Jeong jinyoung <jjyoung2419@gmail.com>
2024-11-28 02:23:40 +0900

    add transaction for create and update in person

--- controller/person.js
+++ controller/person.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
-const Address = require('../models/Address'); // Address 모델 불러오기
+const Address = require('../models/Address');
+const { sequelize } = require('../models');
 
 // 전체 사용자 조회
 const view = async (req, res) => {
@@ -16,6 +17,8 @@ const view = async (req, res) => {
 const create = async (req, res) => {
     const { userId, password, userName, userEmail, is_admin, region, sub_region, street, detail_address, zonecode } = req.body;
 
+    const t = await sequelize.transaction(); // 트랜잭션 시작
+
     // 필수 입력 값 검증
     if (!userId || !password || !userName || !userEmail || !region || !sub_region || !zonecode) {    // 필수 입력값은 region,sub_region,zonecode 까지만
         return res.status(400).json({ message: '필수 입력 값이 누락되었습니다.' });
@@ -28,7 +31,16 @@ const create = async (req, res) => {
         }
 
         // 사용자 생성
-        const newUser = await User.create({ userId, password, userName, userEmail, is_admin });
+        const newUser = await User.create(
+            {
+                userId,
+                password,
+                userName,
+                userEmail,
+                is_admin
+            },
+            { transaction: t } // 트랜잭션 사용
+        );
 
         // 주소 정보 추가
         const newAddress = await Address.create({
@@ -38,23 +50,27 @@ const create = async (req, res) => {
             street,
             detail_address,
             zonecode
-        });
+        },
+        { transaction: t } // 트랜잭션 사용
+    );
 
+    await t.commit(); // 성공 시 트랜잭션 커밋
         res.status(201).json({ message: '회원 및 주소 생성 완료!', user: newUser, address: newAddress });
     } catch (err) {
+        await t.rollback(); // 오류 발생 시 롤백
         console.error(err);
         res.status(500).json({ message: '회원 생성 중 오류 발생.' });
     }
 };
 
-
-// 사용자 수정
 const update = async (req, res) => {
     const { userId, password, userName, userEmail, is_admin, region, sub_region, street, detail_address, zonecode } = req.body;
 
     if (!userId) {
         return res.status(400).json({ message: 'userId는 필수 입력 값입니다.' });
     }
+
+    const t = await sequelize.transaction();
 
     try {
         // 사용자 조회
@@ -64,7 +80,7 @@ const update = async (req, res) => {
         }
 
         // 사용자 정보 수정
-        await user.update({ password, userName, userEmail, is_admin });
+        await user.update({ password, userName, userEmail, is_admin }, { transaction: t });
 
         // 주소 정보 수정
         const address = await Address.findOne({ where: { userId } });
@@ -75,23 +91,24 @@ const update = async (req, res) => {
                 street,
                 detail_address,
                 zonecode
-            });
+            }, { transaction: t });
         } else {
             // 주소가 없으면 새로 생성
             await Address.create({
                 userId,
                 detail_address,
                 zonecode
-            });
+            }, { transaction: t });
         }
 
+        await t.commit();
         res.status(200).json({ message: '회원 및 주소 수정 완료!', user });
     } catch (err) {
+        await t.rollback();
         console.error(err);
         res.status(500).json({ message: '회원 수정 중 오류 발생.' });
     }
 };
-
 
 // 사용자 삭제
 const deleteUser = async (req, res) => {
