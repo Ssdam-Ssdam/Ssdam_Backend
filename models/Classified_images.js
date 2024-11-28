92c4896a1d046533f8bf35b3349d3e202e10793b
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2024-11-28 18:04:02 +0900

    Add: new table Classified_images Feedback Waste_fees

--- models/Classified_images.js
+++ models/Classified_images.js
@@ -1,42 +1,33 @@
 const Sequelize = require('sequelize'); // 쿼리문 쓰지 않고 db 데이터 처리 가능한 모듈
 // config/config.json에서 db 연결 정보 저장
 
-const Inquiry = class Inquiry extends Sequelize.Model {
+const Classified_images = class Classified_images extends Sequelize.Model {
    static init(sequelize) {
       return super.init(
          {
-            inquiryId: {
+            imgId: {
                 type: Sequelize.INTEGER,
                 allowNull: false,
                 unique: true,
                 primaryKey: true,
                 autoIncrement: true
             },
-            title: {
+            file_path: {
                 type: Sequelize.STRING,
                 allowNull: false
             },
-            content: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            created_at: {
+            uploaded_time: {
                 type: Sequelize.DATE,
                 allowNull: false,
                 defaultValue: Sequelize.NOW
             },
-            res_message: {
+            waste_name: {
                 type: Sequelize.STRING,
                 allowNull: true
             },
-            res_date: {
-                type: Sequelize.DATE,
+            accuracy: {
+                type: Sequelize.INTEGER,
                 allowNull: true
-            },
-            res_status: {
-                type: Sequelize.BOOLEAN,
-                allowNull: false,
-                defaultValue: false
             },
             userId: {
                 type: Sequelize.STRING, // User 모델의 기본 키와 동일한 타입으로 설정
@@ -53,8 +44,8 @@ const Inquiry = class Inquiry extends Sequelize.Model {
             sequelize,
             timestamps: false, // createdAt, udaptedAt 자동 생성
             underscored: false, // 카멜 케이스 및 스네이크 케이스 설정(false면 사용자 지정 이름 사용 가능)
-            modelName: 'Inquiry', // 모델명
-            tableName: 'inquiry', // 테이블명
+            modelName: 'Classified_images', // 모델명
+            tableName: 'classified_images', // 테이블명
             paranoid: false, // deletedAt 자동 생성
             charset: 'utf8', // 한글 입력 설정
             collate: 'utf8_general_ci',
@@ -63,12 +54,18 @@ const Inquiry = class Inquiry extends Sequelize.Model {
    }
 
    static associate(db) {
-        db.Inquiry.belongsTo(db.User, {
-            foreignKey: 'userId', // inquiry 테이블의 외래 키
-            targetKey: 'userId',  // User 테이블의 기본 키
-            as: 'UserInquiry',           // 관계를 참조할 때 사용할 별칭
-        });
+    db.Classified_images.belongsTo(db.User, {
+        foreignKey: 'userId', // classified_images 테이블의 외래 키
+        targetKey: 'userId',  // User 테이블의 기본 키
+        as: 'UserClassified_images',           // 관계를 참조할 때 사용할 별칭
+    });
+
+    db.Classified_images.hasMany(db.Feedback, {
+        foreignKey: 'imgId', 
+        sourceKey: 'imgId', 
+        as: 'Feedback', 
+      });
    }
 };
 
-module.exports = Inquiry;
+module.exports = Classified_images;
