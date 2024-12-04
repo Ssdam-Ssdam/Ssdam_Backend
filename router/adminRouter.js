0a667307f53bdcafeb3c616e229dbe413720f1a6
Jeong jinyoung <jjyoung2419@gmail.com>
2024-12-05 03:33:30 +0900

    add logic for retrieving for feedback

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const classifiedImg = require('../controller/classifiedImg'); // controller import
+const classifiedImg = require('../controller/classifiedImg');
+const feedback = require('../controller/feedback');
 const { authenticateAccessToken, authenticateAdmin } = require('../middlewares');
 
 router.get('/classifiedImg/view', authenticateAccessToken, authenticateAdmin, classifiedImg.view);
 
 router.delete('/classifiedImg/delete', authenticateAccessToken, authenticateAdmin, classifiedImg.delete);
+
+router.get('/feedback/view', authenticateAccessToken, authenticateAdmin, feedback.view);
 
 module.exports = router;
