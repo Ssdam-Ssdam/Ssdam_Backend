c67c727040824469c2fb26f8d51cbc86f89539c6
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2024-12-06 01:52:34 +0900

    Add: image classification logic with model prediction and entropy calculation

--- controller/lar_waste.js
+++ controller/lar_waste.js
@@ -1,6 +1,14 @@
 const Feedback = require('../models/Feedback');
 const Waste_fees = require('../models/Waste_fees');
 const Classified_images = require('../models/Classified_images');
+// child-process 사용 모듈
+const { spawn } = require('child_process');
+
+// fastapi 사용 모듈
+const path = require('path');
+const fs = require('fs');
+const FormData = require('form-data');
+const axios = require('axios');
 
 const feedback = async (req, res) => {
     const userId = req.userId;
@@ -61,9 +69,66 @@ const nearbyStores = async (req, res) => {
 }
 
 const upload_img = async (req, res) => {
-    var file = '/img/' + req.file.filename;
+    const userId = req.userId;
+    let file = '/img/' + req.file.filename;
 
+    // select1. fastapi
+    try {
+        const imagePath = path.join(__dirname, '../public/img', req.file.filename);
+        // const imagePath = path.resolve(__dirname, '../input_bed.jpg');  // 이미지 파일 경로
+        const image = fs.createReadStream(imagePath);  // 이미지 파일 스트림
     
+        const form = new FormData();
+        form.append('file', image);
+    
+        const response = await axios.post('http://localhost:8000/predict', form, {
+            headers: {
+                ...form.getHeaders()
+            }
+        });
+    
+        const { predicted_class, entropy } = response.data;
+        
+        const img = await Classified_images.create({
+            file_path: file,
+            waste_name: predicted_class,
+            accuracy: entropy,
+            userId: userId
+        })
+    
+        res.status(200).json({
+            message: "success",
+            img
+        });
+    } catch(err) {
+        console.log(`error: ${err}`);
+        res.status(500).json({
+            error: err,
+            message: "Internal Server Error"
+        })
+    }
+    
+    // select2. child-process 모듈
+    // const image_pth = './input_bed.jpg';
+
+    // const result = spawn('python', ['./return_prediction_entropy_v6.py', image_pth]);
+
+    // result.stdout.on('data', async (data) => {
+    //     let dataString = data.toString().replace(/\r\n/g, '');  // \r\n 제거
+    //     let jsonData = JSON.parse(dataString);  // JSON으로 파싱
+
+    //     console.log(`data: ${JSON.stringify(jsonData)}`);  // JSON 데이터 출력
+
+    //     res.status(200).json({
+    //         message: "success",
+    //         object: jsonData.predicted_class,
+    //         entropy: jsonData.entropy
+    //     });
+    // });
+
+    // result.stderr.on('data', (data) => {
+    //     console.error(`stderr: ${data}`);
+    // });
 }
 
 module.exports = {
--- router/larWasteRouter.js
+++ router/larWasteRouter.js
@@ -9,14 +9,19 @@ const upload = multer({
     storage: multer.diskStorage({
               destination: function (req, file, cb) { cb(null, 'public/img');  },
               filename: function (req, file, cb) {
-                      var newFileName = file.originalname
-                      cb(null, newFileName); }
-             }),
+                // 타임스탬프 생성
+                const timestamp = Date.now();
+                // 파일의 확장자를 유지하여 새 파일 이름 생성
+                const originalName = file.originalname;
+                const extension = originalName.substring(originalName.lastIndexOf('.'));
+                const newFileName = `${timestamp}-${originalName}`;
+                cb(null, newFileName);
+             }}),
     });
 
 router.post('/feedback', authenticateAccessToken, feedback);
 router.get('/search', search);
 router.get('/nearby-stores', nearbyStores);
-router.post('/upload', upload.single('uploadFile'), upload_img);
+router.post('/upload', authenticateAccessToken, upload.single('uploadFile'), upload_img);
 
 module.exports = router;
