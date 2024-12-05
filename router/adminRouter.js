96ba3dfb34560c265d6124315358a9d8bd7fd9fb
Jeong jinyoung <jjyoung2419@gmail.com>
2024-12-05 17:37:01 +0900

    add logic for retrieving for inquiry

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const classifiedImg = require('../controller/classifiedImg');
 const feedback = require('../controller/feedback');
+const inquiry = require('../controller/inquiryAdmin');
 const { authenticateAccessToken, authenticateAdmin } = require('../middlewares');
 
 router.get('/classifiedImg/view', authenticateAccessToken, authenticateAdmin, classifiedImg.view);
@@ -9,5 +10,7 @@ router.get('/classifiedImg/view', authenticateAccessToken, authenticateAdmin, cl
 router.delete('/classifiedImg/delete', authenticateAccessToken, authenticateAdmin, classifiedImg.delete);
 
 router.get('/feedback/view', authenticateAccessToken, authenticateAdmin, feedback.view);
+
+router.get('/inquiry/view', authenticateAccessToken, authenticateAdmin, inquiry.view);
 
 module.exports = router;
