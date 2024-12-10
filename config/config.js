1f26a26ad654bb6b0924a83fa94961d955a1cba2
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2024-12-10 11:23:42 +0900

    database setting update

---
+++ config/config.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_TEST, // 별도 테스트 DB 사용
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+};
+
+// {
+//   "development": {
+//     "username": "root",
+//     "password": "root",
+//     "database": "ssdam",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const env = process.env.NODE_ENV || 'production';
+const config = require('../config/config.js')[env];
 
 // 모델 정의 모듈 불러오기
 const User = require('./User');
